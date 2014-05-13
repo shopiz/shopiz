060790ace0e2c1ff5f432494cb2b769b4d6ddfe5
Jacky Zhang <myself.fervor@gmail.com>
2014-05-13 11:21:14 -0400

    ...

--- src/app_helper/controllers/Import/ShopController.php
+++ src/app_helper/controllers/Import/ShopController.php
@@ -11,7 +11,7 @@ class ShopController extends \BaseController
             'user_id'     => 10001,
             'shop_id'     => 10001,
             'user_nick'   => 'n百万',
-            'session_key' => '6101923c8e08ae018deae939495ff5d1c38f3a219f0200584436708',
+            'session_key' => '6102914b2edcdc63ca9eba1363ea6768456838bc83c734c84436708',
         );
         // $userInfo = array(
             // 'user_id'     => 10002,
@@ -144,61 +144,65 @@ class ShopController extends \BaseController
     public function stockAction()
     {
 
+        // exit;
         $items = new \Api\Taobao\Shop\Items();
         $item_list = $items->getOnsaleList($this->userInfo['session_key']);
         // print_r($item_list);
         // exit;
         $num_iids = array();
+        $goods_ids = array();
         foreach ($item_list as $k => $v) {
             $num_iids[] = $v['num_iid'];
-            // $sql = "SELECT id FROM taobao_shop_goods WHERE shop_id=:shop_id AND num_iid=:num_iid";
-            // $params = array(
-            //     'shop_id' => $this->userInfo['shop_id'],
-            //     'num_iid' => $v['num_iid'],
-            // );
+            $sql = "SELECT goods_id FROM taobao_shop_goods WHERE shop_id=:shop_id AND num_iid=:num_iid";
+            $params = array(
+                'shop_id' => $this->userInfo['shop_id'],
+                'num_iid' => $v['num_iid'],
+            );
 
-            // $goods_info = $this->db->fetchOne($sql, \Phalcon\Db::FETCH_ASSOC, $params);
-            // if (empty($goods_info)) {
-            //     $sql = "INSERT INTO products(product_name, product_price, dateline, lasttime)
-            //             VALUES(:product_name, :product_price, :dateline, :lasttime)";
-            //     $params = array(
-            //         ':product_name' => $v['title'],
-            //         ':product_price' => $v['price'],
-            //         ':dateline' => $_SERVER['REQUEST_TIME'],
-            //         ':lasttime' => $_SERVER['REQUEST_TIME'],
-            //     );
-            //     $this->db->query($sql, $params);
-            //     $product_id = $this->db->lastInsertId();
-            //     $sql = "INSERT INTO user_shop_goods(shop_id, product_id, dateline, lasttime)
-            //             VALUES(:shop_id, :product_id, :dateline, :lasttime)";
-            //     $params = array(
-            //         // 'user_id' => $this->userInfo['shop_id'],
-            //         ':shop_id' => $this->userInfo['shop_id'],
-            //         ':product_id' => $product_id,
-            //         ':dateline' => $_SERVER['REQUEST_TIME'],
-            //         ':lasttime' => $_SERVER['REQUEST_TIME'],
-            //     );
-            //     $this->db->query($sql, $params);
-            //     $goods_id = $this->db->lastInsertId();
-            // } else {
-            //     $goods_id = 0;
-            // }
+            $goods_info = $this->db->fetchOne($sql, \Phalcon\Db::FETCH_ASSOC, $params);
+            if (empty($goods_info)) {
+                $sql = "INSERT INTO products(product_name, product_price, dateline, lasttime)
+                        VALUES(:product_name, :product_price, :dateline, :lasttime)";
+                $params = array(
+                    ':product_name' => $v['title'],
+                    ':product_price' => $v['price'],
+                    ':dateline' => $_SERVER['REQUEST_TIME'],
+                    ':lasttime' => $_SERVER['REQUEST_TIME'],
+                );
+                $this->db->query($sql, $params);
+                $product_id = $this->db->lastInsertId();
+                $sql = "INSERT INTO user_shop_goods(shop_id, product_id, dateline, lasttime)
+                        VALUES(:shop_id, :product_id, :dateline, :lasttime)";
+                $params = array(
+                    // 'user_id' => $this->userInfo['shop_id'],
+                    ':shop_id' => $this->userInfo['shop_id'],
+                    ':product_id' => $product_id,
+                    ':dateline' => $_SERVER['REQUEST_TIME'],
+                    ':lasttime' => $_SERVER['REQUEST_TIME'],
+                );
+                $this->db->query($sql, $params);
+                $goods_id = $this->db->lastInsertId();
+            } else {
+                    $sql = "SELECT goods_id FROM taobao_shop_goods WHERE num_iid=:num_iid";
+                $goods_id = $goods_info['goods_id'];
+            }
+            $goods_ids[$v['num_iid']] = $goods_id;
 
 
-            // $sql = "INSERT INTO taobao_shop_goods(shop_id, goods_id, num_iid, price, title, dateline, lasttime)
-            //         VALUES(:shop_id, :goods_id, :num_iid, :price, :title, :dateline, :lasttime)
-            //         ON DUPLICATE KEY UPDATE price=:price, title=:title, lasttime=:lasttime";
+            $sql = "INSERT INTO taobao_shop_goods(shop_id, goods_id, num_iid, price, title, dateline, lasttime)
+                    VALUES(:shop_id, :goods_id, :num_iid, :price, :title, :dateline, :lasttime)
+                    ON DUPLICATE KEY UPDATE price=:price, title=:title, lasttime=:lasttime";
 
-            // $params = array(
-            //         ':shop_id' => $this->userInfo['shop_id'],
-            //         ':goods_id' => $goods_id,
-            //         ':num_iid' => $v['num_iid'],
-            //         ':price' => $v['price'],
-            //         ':title' => $v['title'],
-            //         ':dateline' => $_SERVER['REQUEST_TIME'],
-            //         ':lasttime' => $_SERVER['REQUEST_TIME'],
-            //     );
-            // $this->db->query($sql, $params);
+            $params = array(
+                    ':shop_id' => $this->userInfo['shop_id'],
+                    ':goods_id' => $goods_id,
+                    ':num_iid' => $v['num_iid'],
+                    ':price' => $v['price'],
+                    ':title' => $v['title'],
+                    ':dateline' => $_SERVER['REQUEST_TIME'],
+                    ':lasttime' => $_SERVER['REQUEST_TIME'],
+                );
+            $this->db->query($sql, $params);
         }
 
         // $item_info = $items->getItemInfo($this->userInfo['session_key'], 2100508935783);
@@ -208,7 +212,44 @@ class ShopController extends \BaseController
         while (count($num_iids) > 0) {
             $ids = array_splice($num_iids, 0, 20);
             $item_list = $items->getMultiItemInfo($this->userInfo['session_key'], implode(',', $ids));
-            print_r($item_list);
+            // print_r($item_list);
+
+            if ($item_list['ok'] == 1) {
+                foreach ($item_list['list'] as $k => $v) {
+                    $sql = "UPDATE products SET product_description=:product_description WHERE goods_id=:goods_id";
+                    $params = array(
+                        ':goods_id' => $goods_ids[$v['num_iid']],
+                        ':product_description' => $v['desc'],
+                    );
+                    $this->db->query($sql, $params);
+
+                    $fields = array(
+                        'approve_status', 'cid', 'seller_cids', 'title', 'num', 'type', 'price',
+                        'detail_url', 'pic_url', 'valid_thru', 'stuff_status', 'product_id',
+                        'item_imgs', 'prop_imgs', 'list_time', 'delist_time',
+                        'ems_fee', 'express_fee', 'post_fee', 'postage_id', 'freight_payer',
+                        'has_discount', 'has_invoice', 'has_showcase', 'has_warranty',
+                        'input_pids', 'input_str', 'props', 'location', 'is_virtual', 'modified',
+                        'outer_id', 'property_alias', 
+                    );
+                    $params = array(
+                        ':num_iid' => $v['num_iid'],
+                    );
+                    $v['outer_id'] = json_encode($v['outer_id']);
+                    $v['property_alias'] = json_encode($v['property_alias']);
+                    foreach ($fields as $kk => $vv) {
+                        $sql_set .= "{$vv}=:{$vv}";
+                    }
+                    $sql = "UPDATE taobao_shop_goods SET
+                            {$sql_set}
+                            WHERE num_iid=:num_iid";
+                    $this->db->query($sql, $params);
+                    echo $sql;exit;
+                }
+            } else {
+                print_r($item_list);
+                exit;
+            }
             exit;
         }
         exit;
